37ad849ca9205eeb81f1fcc66979831a42eb35e0
Josh <pearlsteinj@me.com>
2013-02-18 22:31:17 -0500

    Location App Completed

--- Location/JLPViewController.h
+++ Location/JLPViewController.h
@@ -7,7 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface JLPViewController : UIViewController
-
+@interface JLPViewController : UIViewController <UIAlertViewDelegate, CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, retain) CLLocationManager *locationManager;
 @end
--- Location/JLPViewController.m
+++ Location/JLPViewController.m
@@ -13,11 +13,26 @@
 @end
 
 @implementation JLPViewController
+@synthesize mapView,locationManager;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	locationManager = [[CLLocationManager alloc] init];
+    if([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied){
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Enable Location!"
+                                                       message:@"Please Enable Location Services in Settings to continue."
+                                                      delegate:self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK!", nil];
+        [alert show];
+
+    }
+    locationManager.distanceFilter = 1000;;
+    locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+    locationManager.delegate = self;
+    [locationManager startMonitoringSignificantLocationChanges];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +41,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
+      
+    MKPointAnnotation *pin = [[MKPointAnnotation alloc]init];
+    pin.coordinate = newLocation.coordinate;
+    pin.title = @"Location";
+    [self.mapView addAnnotation:pin];
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, 1000, 1000);
+    [mapView setRegion:region animated:YES];
+}
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Enable Location!"
+                                                   message:@"Please Enable Location in Settings to continue."
+                                                  delegate:self
+                                         cancelButtonTitle:@"Cancel"
+                                         otherButtonTitles:@"OK!", nil];
+    [alert show];
+}
+-(void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+    if([title isEqualToString:@"OK!"]){
+        NSLog(@"OK! button Pressed!");
+    }
+}
 @end
